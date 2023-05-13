56376c654b9035dd0d2b7263d15feea043d2c6cf
Maria José Solano <majosolano99@gmail.com>
2023-05-13 10:10:34 -0700

    Prepare for publish

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # WEC
+
+A collection of useful Rust macros for creating `vec!`s.
+
+## `bec!`
+
+Tired of writing `vec![ Box::new(1), ... Box::new(100) ]`? Me too. So here's macro that does it for you.
+
+```rust
+let v = bec![1, 2, 3];
+assert_eq!(v, vec![Box::new(1), Box::new(2), Box::new(3)]);
+```
+
+## `vinto!`
+
+Calls `.into()` on each element.
+
+```rust
+let v: Vec<String> = vinto!["foo", String::from("bar")];
+assert_eq!(v, vec![String::from("foo"), String::from("bar")]);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+#![warn(missing_docs)]
+
+//! A collection of useful Rust macros for creating `vec!`s.
+
+/// Boxes each element in the vector.
+///
+/// # Examples
+/// ```
+/// use wec::bec;
+///
+/// let v = bec![1, 2, 3];
+/// assert_eq!(v, vec![Box::new(1), Box::new(2), Box::new(3)]);
+/// ```
 #[macro_export]
 macro_rules! bec {
     ( $($elem:expr),* ) => {
@@ -5,6 +18,15 @@ macro_rules! bec {
     };
 }
 
+/// Calls `.into()` on each element in the vector.
+///
+/// # Examples
+/// ```
+/// use wec::vinto;
+///
+/// let v: Vec<String> = vinto!["foo", String::from("bar")];
+/// assert_eq!(v, vec![String::from("foo"), String::from("bar")]);
+/// ```
 #[macro_export]
 macro_rules! vinto {
     ( $($elem:expr),* ) => { ::std::vec![ $( ::std::convert::Into::into($elem) ),* ] };
